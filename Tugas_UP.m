a53495c552e6007649909a684aac648287f6a532
handyarso <99626771+handyarso@users.noreply.github.com>
2022-11-29 12:28:31 +0700

    Update Tugas_UP.m

--- Tugas_UP.m
+++ Tugas_UP.m
@@ -17,3 +17,10 @@ myCoef = myReverse(rEarth, Vpv, Dens)
 
 %myDensity=0.107.*Vpv-0.0535.*Vsv+0.026.*Vpv.*Vsv+0.0463.*(Vpv.^2-1.3333.*(Vsv.^2));
 %myDensity=1.92.*(Vpv.^0.186);
+VpKm = Vpv/1000;
+myDensity = 1.66*VpKm - 0.4721*(VpKm.^2) + 0.0671*(VpKm.^3) - 0.0043*(VpKm.^4) + 0.000106*(VpKm.^5);
+plot(rEarth, myDensity); hold on
+plot(rEarth, Dens./1000, 'r');
+xlabel('Earth Radii (m)','FontSize',12);
+ylabel('Density (gr/cc)','FontSize',12);
+legend('BROCHER et al','PREM','Location','northeast');
